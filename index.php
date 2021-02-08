17834f292b766cf5cc34d5d4381099d26d287b20
yvandouenel <y.douenel@coopernet.fr>
2021-02-08 12:19:39 +0100

    Toute première version de myCMS

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil MyCMS</title>
+</head>
+<body>
+    <p>Première version de mon CMS !!!</p>
+</body>
+</html>
